b9f66dc35d4a6775ca2cb1eab9542290d8738c60
mrunalibind <mrunalibind123@gmail.com>
2022-10-10 19:35:17 +0530

    first commit

---
+++ var1/program4.js
@@ -0,0 +1,16 @@
+const name = "^^^^^^^Mrunali Bind^^^^^^^";
+const school = "Delhi Public School";
+var grade = "A+";
+var section = "B";
+var roll_no = 123;
+var sub1 = 97;
+var sub2 = 93;
+var sub3 = 95;
+console.log("Name:",name);
+console.log("School Name:",school);
+console.log("Grade:",grade);
+console.log("section:",section);
+console.log("Roll no.:",roll_no);
+console.log("");
+console.log("English ---|--- Maths ---|--- Science")
+console.log(sub1, "--------|----", sub2, "-----|-------", sub3)
